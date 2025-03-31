e70e710867362d83bfaf1e7b95d7ef394373a9a7
PatrickL32 <Jedi.92.lewis@gmail.com>
2025-03-30 18:05:23 -0700

    first commit

---
+++ assignment1.js
@@ -0,0 +1,32 @@
+console.log("funnystory");
+//1.Declara Variables//using let
+let Character = "Frank";
+let action = "pet";
+let object = "fluffy pancakes";
+let place = "at a pride parade";
+let time = "on a sunny day";
+
+//Using "const
+const occasion = "Celebrating pride month In june"
+
+//2.create the story
+console.log("---funnystory---")
+console.log(Character + " was dancing " + place + " during the parade " + time + ".");
+console.log("was feeling frisky"+ Character+"flirty"+object+"was in the mood"+action+ ".");
+console.log("was dancing and flirting"+occasion+"at the end of the celebration was good!");
+
+//3.experiment with different values
+
+console.log("---Different verison of the same story---");
+
+//change values to create a different story
+Character= "Pauly";
+action= "looking for a guy";
+object= "a bottle of canadian maple syrup";
+place = "at a pride parade";
+time = "on a sunny day";
+
+console.log(Character +"trying to look for a good seat"+ place +"at a bar"+ time+".");
+console.log("caught a glance from somone" + Character +"looking for anyone in need of" + object+"talked to"+ action+".");
+console.log("he finaly found the guy"+ occasion+"it was a fun filled day!");
+
